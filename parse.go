7a459cab680e98b16801ced317bb041d654f71b7
Harchit Bhatoia <harchit@Harchits-Laptop.local>
2024-02-14 00:09:32 -0500

    first commit

---
+++ parse.go
@@ -0,0 +1,64 @@
+package link
+
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	var links []Link
+
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+		fmt.Println(node)
+	}
+
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+		}
+	}
+	ret.Text = "TODO: parse the text"
+	return ret
+}
+
+// perform dfs on html doc tree to find all <a> elements
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
+
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+" ")
+	}
+}
